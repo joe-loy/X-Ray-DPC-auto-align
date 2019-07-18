2643fde2461afed7f8c1391aa00cf467802f18c7
joe-loy <37566258+joe-loy@users.noreply.github.com>
2019-07-18 15:27:10 -0700

    Update cam.py

--- cam.py
+++ cam.py
@@ -4,6 +4,14 @@ from thorlabs_tsi_sdk.tl_camera import TLCameraSDK, TLCamera, Frame
 from thorlabs_tsi_sdk.tl_camera_enums import SENSOR_TYPE
 from thorlabs_tsi_sdk.tl_mono_to_color_processor import MonoToColorProcessorSDK
 
+"""
+Camera
+
+This class is used to control the CS895MU compact scientific camera
+in order to send over images of the fiducial images produced by the
+laser. 
+
+"""
 class Camera():
 
     def __init__(self, camSesh, cam):
@@ -13,10 +21,11 @@ class Camera():
         camList = camSesh.discover_available_cameras()
         self.cam = camSesh.open_camera('08153')
 
+    # Call destructors for SDK and Camera objects 
     def closeCamera(self):
         self.cam.dispose()
         self.camSesh.dispose()
-
+    # Takes a picture and returns an np.array of the image
     def takePicture(self):
         self.cam.arm()
         temp = self.cam.get_pending_frame_or_null()
