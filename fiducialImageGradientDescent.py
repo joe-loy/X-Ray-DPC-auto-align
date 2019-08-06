5db7061092491b12dad5e698b65c57949c2fab93
joe-loy <37566258+joe-loy@users.noreply.github.com>
2019-08-05 18:51:42 -0700

    Add files via upload

--- fiducialImageGradientDescent.py
+++ fiducialImageGradientDescent.py
@@ -1,27 +1,29 @@
 import numpy as np
-import matplotlib.pyplot as plt
 import scipy as sp
-
+import matplotlib.pylab as plt
 from skimage.io import imread
-from skimage.data import shepp_logan_phantom
+import cv2
 from skimage.transform import radon, rescale
-
-from PIL import Image
 
 
 """
+
 fiducialImageGradientDescent.py
+
 This script is used to implement the functions necessary to perform the
 gradient descent which will be used by the motor controller in order to
 automatically align gratings. The gradient descent will be based on
 two seperate images of radon transforms of the fiducial images, which will
 be used as components of a cost function that will be used to implement
 the gradient descent.
+
 """
 
 
-image = shepp_logan_phantom()
-image = rescale(image, scale=0.4, mode='reflect', multichannel=False)
+
+image = cv2.imread('phantom.png', 0)  # read image in grayscale
+I = image - np.mean(image)
+sinogram = radon(I)
 
 fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(8, 4.5))
 
@@ -38,5 +40,4 @@ ax2.imshow(sinogram, cmap=plt.cm.Greys_r,
 
 fig.tight_layout()
 plt.show()
-
 
